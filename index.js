f037653696614e88404db44977def298e37d93c6
Nathan Cai <nathan97cai@gmail.com>
2023-04-20 14:55:56 -0400

    added put, delete, post

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const { Joke, Op } = require('./db');
+const { Joke, Op } = require("./db");
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/jokes', async (req, res, next) => {
+app.get("/jokes", async (req, res, next) => {
   try {
-    const { tags, content} = req.query;
-    let where = {}
+    const { tags, content } = req.query;
+    let where = {};
 
-    if(tags){
-      where.tags = {[Op.like]: `%${tags}%`}
+    if (tags) {
+      where.tags = { [Op.like]: `%${tags}%` };
     }
 
-    if(content) {
-      where.joke = {[Op.like]: `%${content}%`}
+    if (content) {
+      where.joke = { [Op.like]: `%${content}%` };
     }
 
     const jokes = await Joke.findAll({ where });
@@ -23,7 +23,53 @@ app.get('/jokes', async (req, res, next) => {
     res.send(jokes);
   } catch (error) {
     console.error(error);
-    next(error)
+    next(error);
+  }
+});
+
+app.post("/jokes", async (req, res, next) => {
+  try {
+    const { joke, tags } = req.body;
+    const newJoke = await Joke.create({ joke, tags });
+    res.status(201).json(newJoke);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+app.delete("/jokes:id", async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const joke = await Joke.findByPk(id);
+    if (!joke) {
+      const error = new Error("Joke not found");
+      error.status = 404;
+      throw error;
+    }
+    await joke.destroy();
+    res.status(204).end();
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+app.put("/jokes/:id", async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const { joke, tags } = req.body;
+    const foundJoke = await Joke.findByPk(id);
+    if (!foundJoke) {
+      const error = new Error("Joke not found");
+      error.status = 404;
+      throw error;
+    }
+    const updatedJoke = await foundJoke.update({ joke, tags });
+    res.json(updatedJoke);
+  } catch (error) {
+    console.error(error);
+    next(error);
   }
 });
 
